f5516c1e8e5db8821e0cfb24143893b9d6b92796
Arslaan Qadus <azy2k9@gmail.com>
2021-01-02 17:54:04 +0000

    Added a re-render mechanism

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,27 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { getNamaaz } from './helpers/getTodaysNamaazTimes';
-import { timeFormatter } from './helpers/timeFormatter';
+import { getEpochTime, timeFormatter } from './helpers/timeFormatter';
 import NextNamaaz from './components/NextNamaaz';
 import { Flex, Text } from '@chakra-ui/react';
+import moment from 'moment';
 
 const Home = () => {
+    const [reRenderMechanism, setReRenderMechanism] = useState(moment());
     const namaaz = getNamaaz();
+
+    const currentTime = getEpochTime(moment());
+    const nextNamaazTime = getEpochTime(namaaz.nextNamaaz.time);
+    const whenToReRender = nextNamaazTime - currentTime;
+
+    useEffect(() => {
+        setTimeout(() => {
+            console.log(whenToReRender);
+            setReRenderMechanism(moment());
+        }, whenToReRender);
+    }, [reRenderMechanism, whenToReRender])
 
     return (
         <>
             {namaaz && (
                 <>
                     <Flex bgColor="green.500" w="100%" justifyContent="center" alignItems="center" h="100%" color="white">
-                    {namaaz && namaaz.currentNamaaz.namaaz === 'No Namaaz' ? (
-                        <Text>No Namaaz Right Now</Text>
-                    ) : (
-                        <>
-                            <Text fontSize="4xl" style={{ textAlign: 'center' }}>
-                                {namaaz.currentNamaaz.namaaz}
-                                <Text fontSize="4xl">{timeFormatter(namaaz.currentNamaaz.time)}</Text>
-                            </Text>
-                        </>
-                    )}
+                        {namaaz && namaaz.currentNamaaz.namaaz === 'No Namaaz' ? (
+                            <Text>No Namaaz Right Now</Text>
+                        ) : (
+                                <>
+                                    <Text fontSize="4xl" style={{ textAlign: 'center' }}>
+                                        {namaaz.currentNamaaz.namaaz}
+                                        <Text fontSize="4xl">{timeFormatter(namaaz.currentNamaaz.time)}</Text>
+                                    </Text>
+                                </>
+                            )}
                     </Flex>
                     <Flex bgColor="green.700" w="100%" justifyContent="center" alignItems="center" h="100%" color="white">
                         <NextNamaaz namaaz={namaaz.nextNamaaz} />
--- src/helpers/timeFormatter.ts
+++ src/helpers/timeFormatter.ts
@@ -3,3 +3,7 @@ import moment, { MomentInput } from "moment"
 export const timeFormatter = (time: MomentInput): String => {
     return moment(time).format('h:mm a');
 }
+
+export const getEpochTime = (time: MomentInput): number => {
+    return moment(time).valueOf();
+};
